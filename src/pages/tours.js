562fa7735e895b3fc50bfd12247b85ba9cee4aba
Lawrence <Caulker>
2020-02-27 18:57:12 -0500

    installed and implemented styled components

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import Layout from "../components/Layout"
+import Button from '../styled-component-examples/Button'
 
 class tours extends Component {
   render() {
     return (
-      <Layout>Hello from Tours Page !!!!!</Layout>
+      <Layout>
+        Hello from Tours Page !!!!!
+        <div>
+          <Button>First Button</Button>
+          <Button color="yellow">Second Button</Button>
+        </div>
+      </Layout>
     )
   }
 }
---
+++ src/styled-component-examples/Button.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const color = "blue"
+const Button = styled.button`
+  color: white;
+  background: ${color};
+  font-size: 1rem;
+  font-weight: 300;
+  margin-left: 4px;
+  padding: 0.8rem;
+`
+
+export default Button
