68308a832ae5891617d2b092780b7055141211ae
Lawrence <Caulker>
2020-02-27 08:55:43 -0500

    banner componet completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,22 @@ import React from "react"
 import { Link } from 'gatsby'
 import Layout from "../components/Layout"
 import SimpleHero from '../components/SimpleHero'
+import Banner from "../components/Banner"
 
 export default () => (
   <Layout>
-    <SimpleHero />
+    <SimpleHero>
+      <Banner
+        title="Continue Exploring"
+        info="The ancient palace and fortress of the Moorish rulers of Grenada"
+      >
+        <Link
+          to="/tours"
+          className="btn-white"
+        >
+          Explore Tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
