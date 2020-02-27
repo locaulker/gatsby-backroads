1bdfd827fe59aa95de6e663ddf756f17c1809b14
Lawrence <Caulker>
2020-02-26 21:25:33 -0500

    initial setup of Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../css/navbar.module.css'
+import { FaAlignRight } from 'react-icons/fa'
+import { Link } from 'gatsby'
+import links from '../constants/links'
+import socialIcons from '../constants/social-icons'
+import logo from '../images/logo.svg'
 
 const Navbar = () => {
+  const [isOpen, setNav] = (useState(false))
+
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
+  console.log(isOpen)
   return (
-    <div>
-      <h2>I am the Navbar</h2>
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <img src={logo} alt="Backroads Logo" />
+          <button
+            type="button"
+            className={styles.logoBtn}
+            onClick={toggleNav}
+          >
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+      </div>
+    </nav>
   )
 }
 
--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -1,4 +1,4 @@
-import React from 'react-icons'
+import React from 'react'
 import { FaFacebook, FaTwitterSquare, FaSquarespace } from 'react-icons/fa'
 
 export default [
